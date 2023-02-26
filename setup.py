04cfbee39c283fdd1a97ff736a732366829e0bb2
Helvio Junior <helvio_junior@hotmail.com>
2023-02-26 19:35:29 -0300

    Remove python 3.6

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     package_data={"": ["LICENSE"]},
     data_files=[('', ['requirements.txt'])],
     include_package_data=False,
-    python_requires=">=3.6, <4",
+    python_requires=">=3.7, <4",
     install_requires=requires,
     license=meta["__license__"],
     readme="README.md",
