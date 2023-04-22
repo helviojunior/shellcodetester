2dfa78da0c7b3d5fdcc1d026e63c80c98c19e159
Helvio Junior <helvio_junior@hotmail.com>
2023-04-22 20:20:59 -0300

    Prepare v0.2.7

--- shell_libs/__meta__.py
+++ shell_libs/__meta__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.6'
+__version__ = '0.2.7'
 __title__ = "ShellcodeTester"
 __description__ = "ShellcodeTester is a tool to assembly, compile and test ASM shellcode."
 __url__ = "https://github.com/helviojunior/shellcodetester"
